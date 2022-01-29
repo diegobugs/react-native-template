854c6c7cc457ecddf81a8f6481817713914c2fee
Diego Bugs <diego.f.bugs@gmail.com>
2022-01-29 10:14:03 -0300

    Added redux persist store and bugfixes

--- template/App.tsx
+++ template/App.tsx
@@ -8,23 +8,28 @@
  * @format
  */
 
-import React from 'react';
-import { StatusBar, useColorScheme } from 'react-native';
+import React from "react";
+import { StatusBar, useColorScheme } from "react-native";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { MainNavigator } from '@navigator';
-import { Theme, DarkTheme } from '@utils';
+import { NavigationContainer } from "@react-navigation/native";
+import { MainNavigator } from "@navigator";
+import { Theme, DarkTheme } from "@utils";
+import { Provider } from "react-redux";
+import { store, persistor } from "@store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
 
   return (
-    <>
-      <StatusBar hidden={true} />
-      <NavigationContainer theme={isDarkMode ? DarkTheme : Theme}>
-        <MainNavigator />
-      </NavigationContainer>
-    </>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <StatusBar hidden={true} />
+        <NavigationContainer theme={isDarkMode ? DarkTheme : Theme}>
+          <MainNavigator />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 };
 
--- template/babel.config.js
+++ template/babel.config.js
@@ -1,18 +1,20 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
   plugins: [
     [
-      'module-resolver',
+      "module-resolver",
       {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        root: ["./src"],
+        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
         alias: {
-          '@assets': './src/components/assets',
-          '@atoms': './src/components/atoms',
-          '@molecules': './src/components/molecules',
-          '@navigator': './src/navigator',
-          '@screens': './src/screens',
-          '@utils': './src/utils',
+          "@assets": "./src/assets",
+          "@atoms": "./src/components/atoms",
+          "@molecules": "./src/components/molecules",
+          "@navigator": "./src/navigator",
+          "@organisms": "./src/components/organisms",
+          "@store": "./src/store",
+          "@screens": "./src/screens",
+          "@utils": "./src/utils",
         },
       },
     ],
