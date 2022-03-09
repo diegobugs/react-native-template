bcb37295882a3a99444896c6b22caacd7a58c735
Diego Bugs <diego.f.bugs@gmail.com>
2022-03-09 14:55:59 -0300

    Change app name

--- template/ios/DemoRNTemplateTests/DemoRNTemplateTests.m
+++ template/ios/DemoRNTemplateTests/DemoRNTemplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DemoRNTemplateTests : XCTestCase
+@interface RNTemplateTests : XCTestCase
 
 @end
 
-@implementation DemoRNTemplateTests
+@implementation RNTemplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
